04f8e8c58d94cea50227a11d44291dd98a690050
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-05-25 23:30:43 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # deVox
 Voxel (EMCoS Studio .inp Voxel Text File) to .obj (Wavefront) converter
+
+## Installation
+
+First, run the following command to install the dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+And then clone the repository.
+```bash
+git clone git@github.com:memory-hunter/deVox.git
+```
+
+## Usage
+
+```t
+deVox.py [-h] -i INPUT
+
+Convert a voxel file to an obj file.
+
+options:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input voxel file.
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ deVox.py
@@ -0,0 +1,70 @@
+import argparse as ap
+
+parser = ap.ArgumentParser(description='Convert a voxel file to an obj file.')
+parser.add_argument('-i', '--input', help='Input voxel file.', required=True)
+args = parser.parse_args()
+
+voxelFile = args.input
+
+print('Converting voxel file to obj file...')
+
+with open(voxelFile, 'r') as f:
+    lines = f.readlines()
+
+    # Get the dimensions of the voxel file
+    dims = lines[0].split()
+    dims = [int(x) for x in dims]
+    print("There are {} vertices and {} faces in the voxel file.".format(dims[0], dims[1]))
+
+    # Get the voxel data
+    voxelData = lines[1:]
+
+# Parse the vertices and faces
+
+i = 0
+objLines = []
+
+while(i <= 2*dims[0]-1):
+    line1 = voxelData[i].split()
+    line2 = voxelData[i+1].split()
+
+    print("Vertex {}: ".format(line1[0]), end="")
+
+    line1 = [float(x.strip()) for x in line1]
+    line1 = [line1[1], line1[2]]
+    line2 = [float(x.strip()) for x in line2]
+
+    print("x: {}, y: {}, z: {}".format(line1[0], line1[1], line2[0]))
+
+    objLines.append("v {} {} {}\n".format(line1[0], line1[1], line2[0]))
+
+    i += 2
+
+voxelData = voxelData[2*dims[0]:]
+i = 0
+
+while(i <= 2*dims[1]-1):
+    line1 = voxelData[i].split()
+    line2 = voxelData[i+1].split()
+
+    print("Faces {}: ".format(line1[0]), end="")
+
+    line1 = line1[3:]
+    line1 = [int(x.strip()) for x in line1]
+    line2 = [int(x.strip()) for x in line2]
+
+    print("{} {} {} {}".format(line1[0], line1[1], line1[2], line1[3]), end=", ")
+    print("{} {} {} {}".format(line2[0], line2[1], line2[2], line2[3]))
+
+    objLines.append("f {} {} {} {}\n".format(line1[0], line1[1], line1[2], line1[3]))
+    objLines.append("f {} {} {} {}\n".format(line2[0], line2[1], line2[2], line2[3]))
+
+    i += 2
+    pass
+
+print("\nWriting obj file...")
+
+with open('voxel.obj', 'w') as f:
+    f.writelines(objLines)
+
+print("Done!")
